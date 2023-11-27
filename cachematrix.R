260199ecce81a264f4abc7db4b811e5626930f7c
arun96NIN <148255635+arun96NIN@users.noreply.github.com>
2023-11-27 18:35:52 +0530

    Add files via upload
    Final output for Peer-graded Assignment: Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,47 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  inverse <- NULL
+  
+  set <- function(matrix) {
+    mat <<- matrix
+    inverse <<- NULL
+  }
+  
+  get <- function() mat
+  
+  setinverse <- function(inv) {
+    inverse <<- inv
+  }
+  
+  getinverse <- function() inverse
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(cacheMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cached_inverse <- cacheMatrix$getinverse()
+  
+  if (!is.null(cached_inverse)) {
+    message("Getting cached inverse")
+    return(cached_inverse)
+  }
+  
+  matrix_to_invert <- cacheMatrix$get()
+  inv <- solve(matrix_to_invert, ...)
+  
+  cacheMatrix$setinverse(inv)
+  
+  inv
+  
 }
+
+#inversed matrix as output
+
+mat <- makeCacheMatrix(matrix(c(1, 2, 3, 4), nrow = 2))
+cacheSolve(mat)
